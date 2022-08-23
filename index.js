f0e9264ba7b0fa25cfd6dae025fa5dd9ae81902f
Guilherme Lopes <guilhermxlopes@gmail.com>
2022-08-23 16:42:25 -0300

    Getting data from form

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.post("/cad", (req, res) => {
+  res.send(req.body);
 });
 
 app.listen(port, () => {
