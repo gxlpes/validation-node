ddb492f7c64de0aba3fc61839f7c0c2c7e538c34
Guilherme Lopes <guilhermxlopes@gmail.com>
2022-08-24 00:23:15 -0300

    Validação feita

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const session = require("express-session");
 const app = express();
 const port = 3000;
 
@@ -8,12 +9,66 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(__dirname + "/public"));
 
+// config session
+app.use(
+  session({
+    secret: "ChaveSegredo",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/index.html"));
 });
 
 app.post("/cad", (req, res) => {
-  res.send(req.body);
+  // getting input form data
+  let name = req.body.name;
+  let email = req.body.email;
+  let exp = req.body.exp;
+  let comments = req.body.comments;
+
+  // storing errors
+  const errors = [];
+
+  // cleaning inputs
+  name = name.trim();
+  email = email.trim();
+  name = name.replace(/[^A-zÀ-ú\s]/gi, "");
+
+  // verify empty name
+  if (name == " " || typeof name == undefined || name == null) {
+    errors.push({ msg: "Empty input" });
+  }
+
+  // checking if name is valid just letters
+  if (!/^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ\s]+$/.test(name)) {
+    errors.push({ msg: "Invalid name" });
+  }
+
+  // checking email
+  if (email == " " || typeof email == undefined || email == null) {
+    errors.push({ msg: "Empty email input" });
+  }
+
+  // checking if email is valid
+  if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+    errors.push({ msg: "Email input invalid" });
+  }
+
+  // checking errors display
+  if (errors.length > 0) {
+    console.log(errors);
+    req.session.errorsSession = errors;
+    req.session.successSession = false;
+    return res.redirect("/");
+  }
+
+  // if success
+  console.log("Validação feita!");
+  req.session.successSession = true;
+  return res.redirect("/");
 });
 
 app.listen(port, () => {
